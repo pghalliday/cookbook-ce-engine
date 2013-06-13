fdefa8759791273b11fd12a8c21142860f565be5
Peter Halliday <pghalliday@gmail.com>
2013-06-13 14:47:04 +0200

    build vm faster (fix node.js version later)

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,7 @@ default[:ce_engine][:ce_operation_hub_subscriber] = "tcp://localhost:4001"
 default[:ce_engine][:ce_operation_hub_push] = "tcp://localhost:4002"
 
 # dependency defaults
-default[:nodejs][:install_method] = "source"
-default[:nodejs][:version] = "0.10.9"
+default[:nodejs][:install_method] = "package"
 default[:git][:version] = "1.8.3"
 default[:zeromq][:version] = "3.2.3"
 default[:zeromq][:url] = "http://download.zeromq.org"
